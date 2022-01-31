967695b57333800db6c37af78c9bf2ed2062516f
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-01-31 16:23:42 -0600

    Initial Commit

---
+++ Shared/Quadratic_EquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quadratic_EquationApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Quadratic_EquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
